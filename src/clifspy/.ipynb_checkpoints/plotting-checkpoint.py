Fixed noise/covariance bugs, other minor updates

src/clifspy/.ipynb_checkpoints/plotting-checkpoint.py
@@ -1,6 +1,6 @@
 import  os
 from astropy.stats import sigma_clipped_stats
-from photutils.aperture import SkyEllipticalAperture, EllipticalAperture
+from photutils.aperture import SkyEllipticalAperture
 from astropy.coordinates import SkyCoord
 from matplotlib.patches import Circle, RegularPolygon
 from matplotlib.collections import PatchCollection
@@ -61,20 +61,6 @@ class panel_image:
         dec_offset.set_ticklabel_position("l")
         if grid:
             overlay.grid(color = "k", alpha = 0.1, lw = 0.5)
-
-    def _get_rmask(self):
-        x0, y0 = WCS(self.maps[1].header).celestial.world_to_pixel(self.galaxy.c)
-        cd = self.maps[1].header["PC2_2"]
-        imshape = self.maps[1].data.shape[1:]
-        #yy, xx = np.mgrid[:imshape[0], :imshape[1]]
-        r90_px = self.galaxy.config["galaxy"]["r90"] / 3600 / cd
-        ba = 1 - self.galaxy.config["galaxy"]["ell"]
-        th = np.radians(self.galaxy.config["galaxy"]["pa"] - 90)
-        #r = np.sqrt((x0 - xx)**2 + (y0 - yy)**2)
-        aper = EllipticalAperture((x0, y0), r90_px, ba*r90_px, theta=th)
-        apermask = aper.to_mask(method="center")
-        mask_im = apermask.to_image(imshape).astype(bool)
-        return ~mask_im
 
     def optical(self, gax, rgb = False, xlim = None, ylim = None):
         img, img_h = self.galaxy.get_cutout_image("cfht", "G", header = True)
@@ -138,7 +124,6 @@ class panel_image:
         vel = self.maps["STELLAR_VEL"].data
         vel[self.maps["BINID"].data[0] == -1] = np.nan
         vel[self.maps["BIN_SNR"].data < 8] = np.nan
-        #vel[self._get_rmask()] = np.nan
         if mask is not None:
             vel[~mask] = np.nan
         ax = self.fig.add_subplot(gax, projection = WCS(self.maps[1].header, naxis = 2))
@@ -160,11 +145,10 @@ class panel_image:
         self._offset_axis(ax, labels = False, grid = True)
         ax.set_facecolor("#dddddd")
 
-    def vdisp_star(self, gax, mask=None, xlim=None, ylim=None, xticks=False, yticks=False):
+    def vdisp_star(self, gax, mask = None, xlim = None, ylim = None, xticks = False, yticks = False):
         vel = self.maps["STELLAR_SIGMA"].data
         vel[self.maps["BINID"].data[0] == -1] = np.nan
         vel[self.maps["BIN_SNR"].data < 8] = np.nan
-        #vel[self._get_rmask()] = np.nan
         if mask is not None:
             vel[~mask] = np.nan
         ax = self.fig.add_subplot(gax, projection = WCS(self.maps[1].header, naxis = 2))
@@ -298,7 +282,7 @@ class panel_image:
         for i in range(len(self.panels)):
             r = i // 3
             c = (i % 3) + 2
-            getattr(self, self.panels[i])(self.axis_grid[r, c], xlim=xlim, ylim=ylim)
+            getattr(self, self.panels[i])(self.axis_grid[r, c], xlim = xlim, ylim = ylim)
         self.fig.savefig(filepath + ".png", bbox_inches = "tight", pad_inches = 0.03)
         self.fig.savefig(filepath + ".pdf", bbox_inches = "tight", pad_inches = 0.03)
 
@@ -348,7 +332,7 @@ def specfit(galaxy):
     ax.axvspan(lgap_red[0], lgap_red[1], color = "k", lw = 0, alpha = 0.2)
     ax.plot(wave, spec, color = "k", lw = 1.0, drawstyle = "steps")
     ax.plot(wave_model, model_spec, color = "C0", lw = 0.8, drawstyle = "steps")
-    ax.set_xlim(3725, 9400)
+    ax.set_xlim(3725, 9300)
     ax.text(0.015, 0.97, r"CLIFS {}:$\;\;$({}, {})".format(galaxy.clifs_id, x, y), fontsize = 9,
                ha = "left", va = "top", transform = ax.transAxes)
 
@@ -366,8 +350,6 @@ def specfit(galaxy):
         axin = ax.inset_axes([0.25, 0.55, 0.6, 0.4],
             xlim = (3732 * (1 + zgal) * (1 + vel / c), 4000 * (1 + zgal) * (1 + vel / c)),
             ylim = galaxy.config["plotting"]["specfit"]["inset_ylim"], yticklabels = [])
-        axin.fill_between(wave[mask], spec[mask] - spec_err[mask], spec[mask] + spec_err[mask],
-            color="k", lw=0, alpha=0.25, step="pre")
         axin.plot(wave[mask], spec[mask], color = "k", lw = 1.0, drawstyle = "steps")
         axin.plot(wave_model[mask_model], model_spec[mask_model], color = "C0", lw = 0.8, drawstyle = "steps")
         axin.set_yticks([])

src/clifspy/cube.py
@@ -28,11 +28,11 @@ def _do_nothing():
     pass
 
 def _ivar_sum(arr, axis):
-    N = 2
-    alpha = 1.28
+    N = 4
+    alpha = 1.7
     noise_correction = 1.0 + alpha * np.log10(N)
     var = 1 / arr
-    new_var = noise_correction * np.nansum(var, axis = axis)
+    new_var = noise_correction * np.nansum(var, axis=axis)
     return 1 / new_var
 
 def _standard_err_mean(arr, axis):
@@ -94,7 +94,7 @@ def preprocess_cube(galaxy, fname, hdul, arm, ext_data=1, ext_ivar=2, ext_fluxca
     cal_ivar = hdul[ext_ivar].data * (1 / hdul[ext_fluxcal].data[:, None, None] ** 2)
     wcs = WCS(hdul[ext_data].header)
     cdelt = hdul[ext_data].header["CD2_2"]
-    wave_orig = utils.wave_axis_from_wcs(wcs, cal_data.shape[0])
+    wave_orig = utils.spectral_axis_from_wcs(wcs, cal_data.shape[0])
     cal_data, cal_ivar = fibre_to_pixel_flux_conversion(cal_data, cal_ivar, cdelt)
     if galaxy.config["cube"]["xmin"] == -99:
         fullfield = True
@@ -102,8 +102,10 @@ def preprocess_cube(galaxy, fname, hdul, arm, ext_data=1, ext_ivar=2, ext_fluxca
         fullfield = False
     else:
         raise ValueError("xmin should either = -99 or be >= 0")
-    if galaxy.config["pipeline"]["bkgsub"]: cal_data = bkg_sub(galaxy, cal_data, wave_orig, wcs)
-    if not fullfield: cal_data, cal_ivar = crop_cube(galaxy, cal_data, cal_ivar)
+    if galaxy.config["pipeline"]["bkgsub"]:
+        cal_data = bkg_sub(galaxy, cal_data, wave_orig, wcs)
+    if not fullfield:
+        cal_data, cal_ivar = crop_cube(galaxy, cal_data, cal_ivar)
     if galaxy.config["pipeline"]["downsample_wav"]:
         wave, cal_data = downsample_wav_axis(wave_orig, cal_data, "flux", return_wave = True)
         cal_ivar = downsample_wav_axis(wave_orig, cal_ivar, "ivar")
@@ -153,7 +155,7 @@ def bkg_sub(galaxy, data, wave, wcs):
     data = data - bkg_cube
     return data
 
-def downsample_wav_axis(wave, data, method, return_wave=False):
+def downsample_wav_axis(wave, data, method, return_wave=False, cov=0):
     new_data = np.zeros((data.shape[0] // 2, data.shape[1], data.shape[2]))
     new_wave = np.zeros(new_data.shape[0])
     k = 0
@@ -161,7 +163,7 @@ def downsample_wav_axis(wave, data, method, return_wave=False):
         if method == "flux":
             new_data[i, :, :] = 0.5 * (data[k, :, :] + data[k + 1, :, :])
         elif method == "ivar":
-            var = 0.5 * (1 / data[k, :, :] + 1 / data[k + 1, :, :])
+            var = (1/4) * (1/data[k, :, :] + 1/data[k + 1, :, :] + 2*cov)
             new_data[i, :, :] = 1 / var
         else:
             raise ValueError("Invalid method keyword provided to 'downsample_wav_axis'")
@@ -184,7 +186,7 @@ def downsample_cube_spatial(cube, axes, cube_type, factor=2):
 def reproject_spectral_axis(cube, l_low, l_high, dl, fill_value=0):
     dl_ang = dl.to(units.AA).value
     new_spectral_axis = np.arange(l_low.to(units.AA).value, l_high.to(units.AA).value + dl_ang / 2, dl_ang) * units.AA
-    cube_newspec = cube.spectral_interpolate(new_spectral_axis, fill_value = fill_value, update_function = _do_nothing)
+    cube_newspec = cube.spectral_interpolate(new_spectral_axis, fill_value = fill_value, update_function=None)
     return cube_newspec
 
 def stitch_cubes(cube_blue, cube_red, ivar_blue, ivar_red):
@@ -226,8 +228,9 @@ def write_fullcube(galaxy, fname_out, fname_blue, fname_red, config, cube_full, 
         logger.info("Converted cube to HDF5")
 
 def generate_cube(galaxy, fullfield=False):
-    if galaxy.config["pipeline"]["downsample_spatial"] and ((galaxy.config["pipeline"]["alpha"] is None) | (galaxy.config["pipeline"]["factor_spatial"] is None)):
-        raise ValueError("If 'downsample_spatial = True', 'alpha', 'factor' cannot be None")
+    if galaxy.config["pipeline"]["downsample_spatial"] and (galaxy.config["pipeline"]["factor_spatial"] is None):
+        raise ValueError("If 'downsample_spatial = True', 'factor' cannot be None")
+    outdir = galaxy.config["files"]["outdir"]
     fname_blue = galaxy.config["files"]["cube_blue"]
     fname_red = galaxy.config["files"]["cube_red"]
     hdul_blue = fits.open(fname_blue)
@@ -248,14 +251,19 @@ def generate_cube(galaxy, fullfield=False):
         ivar_blue = downsample_cube_spatial(ivar_blue, [1, 2], "ivar", factor = galaxy.config["pipeline"]["factor_spatial"])
         ivar_red = downsample_cube_spatial(ivar_red, [1, 2], "ivar", factor = galaxy.config["pipeline"]["factor_spatial"])
         logger.info("Done spatial binning")
-    cube_blue = reproject_spectral_axis(cube_blue, 3700 * units.AA, 9000 * units.AA, cube_blue.header["CDELT3"] * units.m)
-    cube_red = reproject_spectral_axis(cube_red, 3700 * units.AA, 9000 * units.AA, cube_blue.header["CDELT3"] * units.m)
-    ivar_blue = reproject_spectral_axis(ivar_blue, 3700 * units.AA, 9000 * units.AA, cube_blue.header["CDELT3"] * units.m)
-    ivar_red = reproject_spectral_axis(ivar_red, 3700 * units.AA, 9000 * units.AA, cube_blue.header["CDELT3"] * units.m)
+    wavblue = cube_blue.spectral_axis.to(units.AA).value
+    min = np.abs(wavblue - 3700).argmin()
+    wavred = cube_red.spectral_axis.to(units.AA).value
+    max = np.abs(wavred - 9400).argmin()
+    minlam = wavblue[min]
+    maxlam = wavred[max]
+    cube_blue = reproject_spectral_axis(cube_blue, minlam*units.AA, maxlam*units.AA, cube_blue.header["CDELT3"]*units.m)
+    cube_red = reproject_spectral_axis(cube_red, minlam*units.AA, maxlam*units.AA, cube_blue.header["CDELT3"]*units.m)
+    ivar_blue = reproject_spectral_axis(ivar_blue, minlam*units.AA, maxlam*units.AA, cube_blue.header["CDELT3"]*units.m)
+    ivar_red = reproject_spectral_axis(ivar_red, minlam*units.AA, maxlam*units.AA, cube_blue.header["CDELT3"]*units.m)
     logger.info("Reprojected red and blue cubes onto common spectral axis")
     cube_full, ivar_full = stitch_cubes(cube_blue, cube_red, ivar_blue, ivar_red)
     logger.info("Combined red and blue cubes")
-    outdir = galaxy.config["files"]["outdir"]
     if galaxy.config["pipeline"]["downsample_spatial"]:
         if fullfield:
             outfile = outdir + "/calibrated_cube_full.fits"

src/clifspy/dap.py
@@ -49,7 +49,7 @@ class WEAVEDataCube(DataCube):
         # Resample to a geometric sampling
         # - Get the wavelength vector
         spatial_shape = flux.shape[1:][::-1]
-        wave = clifspy.utils.wave_axis_from_wcs(wcs, flux.shape[0]) * units.AA
+        wave = clifspy.utils.spectral_axis_from_wcs(wcs, flux.shape[0]) * units.AA
         # - Convert wavelengths to vacuum
         wlum = wave.to(units.um).value
         wave = ((1+1e-6*(287.6155+1.62887/wlum**2+0.01360/wlum**4)) * wave).to(units.AA).value
@@ -82,15 +82,16 @@ class WEAVEDataCube(DataCube):
         head_new["CRPIX3"] = (1.0, "Pixel coordinate of reference point")
         wcs_new = WCS(head_new)
         ivar = r.oute.reshape(*spatial_shape, -1)
+        flux = r.outy.reshape(*spatial_shape, -1)
         mask = r.outf.reshape(*spatial_shape, -1) < 0.8
-        ivar[mask] = 0.0
+        mask = mask | (~np.isfinite(ivar)) | (~np.isfinite(flux))
         gpm = np.logical_not(mask)
         ivar[gpm] = 1 / ivar[gpm]**2
-        ivar[~np.isfinite(ivar)] = 0.0
-        _sres = np.full(ivar.shape, sres, dtype=float)
-        flux = r.outy.reshape(*spatial_shape, -1)
+        ivar[mask] = 0.0
         flux[mask] = 0.0
-        flux[~np.isfinite(flux)] = 0.0
+        #ivar[(~np.isfinite(ivar))] = 0.0
+        _sres = np.full(ivar.shape, sres, dtype=float)
+        #flux[~np.isfinite(flux)] = 0.0
         # Default name assumes file names like, e.g., '*_icubew.fits'
         super().__init__(flux, ivar=ivar, mask=mask, sres=_sres,
                          wave=r.outx, meta=meta, prihdr=head_new, wcs=wcs_new,